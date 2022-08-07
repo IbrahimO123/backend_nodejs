88598ef3193abd801612af41242801a5ecaf25e0
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2022-08-07 05:09:57 +0100

    "my first node js commit with Mosh"

---
+++ app.js
@@ -0,0 +1,23 @@
+const Logger = require('./eventsEmitter')
+const EventEmitter = require('events')
+const path = require('path')
+const fs = require('fs')
+let nameUser = "Ibrahim"
+function callUserName( nameUser) {
+    console.log(nameUser)
+} 
+// const files = fs.readdirSync('./')
+// console.log(files)
+
+const logger = new Logger();  
+
+// const file = fs.readdir('.', (err, files) => {
+//    if (err) { return console.log('Error: ' + err)}
+//    else { return console.log(files) }
+// })
+
+
+//Listen to an events for messageLogger
+logger.on('messageLogger', (arg)=> console.log(arg))
+
+logger.log('logger should be initialized')
